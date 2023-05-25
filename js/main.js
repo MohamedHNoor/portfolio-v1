90401be5482a066cb439bae6105e0b9b6ab6f547
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-25 21:10:57 +0200

    Add slider functionality

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const navbar = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-btn');
 const closeBtn = document.querySelector('#close-btn');
 const sidebar = document.querySelector('#sidebar');
-const date = document.querySelector('#date');
+// const date = document.querySelector('#date');
 
 // add fixed class to navbar
 window.addEventListener('scroll', () => {
@@ -21,4 +21,43 @@ closeBtn.addEventListener('click', () => {
   sidebar.classList.remove('show-sidebar');
 });
 // set year
-date.innerHTML = new Date().getFullYear();
+// date.innerHTML = new Date().getFullYear();
+
+// project slider
+
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+
+const slider = () => {
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = 'block';
+  } else {
+    nextBtn.style.display = 'none';
+  }
+  if (counter > 0) {
+    prevBtn.style.display = 'block';
+  } else {
+    prevBtn.style.display = 'none';
+  }
+
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+
+nextBtn.addEventListener('click', () => {
+  counter += 1;
+  slider();
+});
+
+prevBtn.addEventListener('click', () => {
+  counter -= 1;
+  slider();
+});
