f657ca573b2ef1ea3fa860fcfa6fe01ea6a1fddd
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2023-05-26 02:23:29 +0200

    Add arrays for projects

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    id: 1,
+    img: 'https://www.course-api.com/images/people/person-1.jpeg',
+    name: 'susan doe',
+    tecks: ['ruby on rails', 'react', 'redux', 'JavaScript', 'PostgreSQL'],
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?',
+    live: 'link',
+    code: 'github link',
+  },
+  {
+    id: 2,
+    img: 'https://www.course-api.com/images/people/person-2.jpeg',
+    name: 'susan doe',
+    tecks: ['ruby on rails', 'react', 'redux', 'JavaScript', 'PostgreSQL'],
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?',
+    live: 'link',
+    code: 'github link',
+  },
+  {
+    id: 3,
+    img: 'https://www.course-api.com/images/people/person-3.jpeg',
+    name: 'susan doe',
+    tecks: ['ruby on rails', 'react', 'redux', 'JavaScript', 'PostgreSQL'],
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?',
+    live: 'link',
+    code: 'github link',
+  },
+];
+
+export default projects;
